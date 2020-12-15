02c2869afdadd678bb632a8f7bf7e5389d3615bd
pLm-k <64797647+pLm-k@users.noreply.github.com>
2020-12-16 00:30:56 +0100

    Add files via upload

---
+++ src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/teacher/BuggyHamsterGame.java
@@ -0,0 +1,29 @@
+package de.unistuttgart.iste.rss.oo.sheet09.teacher;
+
+/**
+ * Hamster game which contains a bug. It's being used to demonstrate the debugging tools provided by eclipse.
+ */
+public class BuggyHamsterGame extends SimpleHamsterGame {
+
+    public BuggyHamsterGame() {
+        game.initialize();
+        game.displayInNewGameWindow();
+    }
+
+    @Override
+    protected void run() {
+        // TODO: Place breakpoint marker in NEXT line.
+        paule.move();
+        paule.move();
+        paule.pickGrain();
+        paule.pickGrain();
+        paule.move();
+    }
+
+    public static void main(String[] args) {
+        BuggyHamsterGame buggyHamsterGame = new BuggyHamsterGame();
+        buggyHamsterGame.game.startGame(false);
+        buggyHamsterGame.doRun();
+        buggyHamsterGame.game.stopGame();
+    }
+}
