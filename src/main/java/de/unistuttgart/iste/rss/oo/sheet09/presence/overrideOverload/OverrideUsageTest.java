0dfcb65bda820f7ab1f09e469ed1bf20a81ae4d5
pLm-k <64797647+pLm-k@users.noreply.github.com>
2020-12-15 20:17:41 +0100

    Update OverrideUsageTest.java

--- src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/overrideOverload/OverrideUsageTest.java
+++ src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/overrideOverload/OverrideUsageTest.java
@@ -2,8 +2,16 @@ package de.unistuttgart.iste.rss.oo.sheet09.presence.overrideOverload;
 
 public class OverrideUsageTest {
 
-    public static void main(final String[] args) {
-    	 // Create an entity libraryOfAlexandria and stuttgartOperaHouse of type Library and Opera and reference a new library and opera respectively
-         // Call print building info on both of these entities
-    }
+	public static void main(final String[] args) {
+		// Create an entity libraryOfAlexandria and stuttgartOperaHouse of type Library
+		// and Opera
+		// Let reference a new library and opera respectively
+		
+        // Call print building info on both of these entities
+		
+		// Create entities building1 and building2 of type Building
+		// let them reference libraryOfAlexandria and stuttgartOperaHouse
+		
+        // Call print building info on both of these entities
+	}
 }
