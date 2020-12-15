37d9c6ad0fa9e842a5352381d820309104a1fa44
pLm-k <64797647+pLm-k@users.noreply.github.com>
2020-12-15 20:16:43 +0100

    Update OverloadedUsageTest.java

--- src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/overrideOverload/OverloadedUsageTest.java
+++ src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/overrideOverload/OverloadedUsageTest.java
@@ -2,27 +2,33 @@ package de.unistuttgart.iste.rss.oo.sheet09.presence.overrideOverload;
 
 public class OverloadedUsageTest {
 
-    public static void main(final String[] args) {
-        // Create an entity libraryOfAlexandria and stuttgartOperaHouse of type Library and Opera and reference a new library and opera respectively
-        
-        // Call this class' static print building info and pass libraryOfAlexandria and stuttgartOperaHouse
-        
-        // Create entities building1 and building2 of type Building and let them reference libraryOfAlexandria and stuttgartOperaHouse
-        // Call this class' static print building info and pass each of the building entities
-    }
-    
-    private static void printBuildingInfo(final Building building) {
-        System.out.format("My type is Building\n");
-        System.out.format("My name is %s\n", building.getBuildingName());        
-    }
+	public static void main(final String[] args) {
+		// Create an entity libraryOfAlexandria and stuttgartOperaHouse of type Library
+		// and Opera
+		// Let them reference a new library and opera respectively
 
-    private static void printBuildingInfo(final Library aLibrary) {
-        System.out.format("My type is Library\n");
-        System.out.format("My name is %s\n", aLibrary.getBuildingName());        
-    }
+		// Call this class' static print building info and pass libraryOfAlexandria and
+		// stuttgartOperaHouse
 
-    private static void printBuildingInfo(final Opera aOpera) {
-        System.out.format("My type is Opera\n");
-        System.out.format("My name is %s\n", aOpera.getBuildingName());        
-    }
+		// Create entities building1 and building2 of type Building
+		// let them reference libraryOfAlexandria and stuttgartOperaHouse
+		
+        // Call this class' static print building info and pass each of the building
+		// entities
+	}
+
+	private static void printBuildingInfo(final Building building) {
+		System.out.format("My type is Building\n");
+		System.out.format("My name is %s\n", building.getBuildingName());
+	}
+
+	private static void printBuildingInfo(final Library aLibrary) {
+		System.out.format("My type is Library\n");
+		System.out.format("My name is %s\n", aLibrary.getBuildingName());
+	}
+
+	private static void printBuildingInfo(final Opera aOpera) {
+		System.out.format("My type is Opera\n");
+		System.out.format("My name is %s\n", aOpera.getBuildingName());
+	}
 }
