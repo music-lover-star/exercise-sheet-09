833c077d34e18a5133873f9a47deab265c301c9a
marxlaml <miguel@marxlarre.net>
2020-12-17 10:40:30 +0100

    Added class with main method for exercise 2

---
+++ src/main/java/de/unistuttgart/iste/rss/oo/sheet09/presence/polymorphismCollections/ListTester.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.iste.rss.oo.sheet09.presence.polymorphismCollections;
+
+import java.util.*;
+
+/**
+ * A class to test list implementations
+ */
+class ListTester {
+
+	public static void main(String[] args) {
+		//Implement the tests from exercise 2 here.
+	}
+}
