8bdcd9ec04a5c738c1517482c9b1dbc688fde474
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-12-05 11:22:30 +0100

    add a game in case anyone wants to try stuff out.

---
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/homework/JustSomeHamsterGameApp.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.iste.sqa.pse.sheet09.homework;
+
+/**
+ * Just some app to start some game.
+ * 
+ * @author your name
+ */
+public final class JustSomeHamsterGameApp {
+	public static void main(String[] args) {
+		final JustSomeHamsterGame game = new JustSomeHamsterGame();
+		game.doRun();
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+open module exercise {
+	exports de.unistuttgart.iste.sqa.pse.sheet09.homework;
+
+	requires de.hamstersimulator.objectsfirst.main;
+}
