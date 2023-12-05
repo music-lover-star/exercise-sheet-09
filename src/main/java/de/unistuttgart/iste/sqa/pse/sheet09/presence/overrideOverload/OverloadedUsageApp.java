e1aac51959d4cb5e46ee5fdaf9da493f00d3316f
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-12-05 11:21:22 +0100

    provide implementation for override/overload to focus on theory.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/presence/overrideOverload/OverloadedUsageApp.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/presence/overrideOverload/OverloadedUsageApp.java
@@ -6,17 +6,24 @@ package de.unistuttgart.iste.sqa.pse.sheet09.presence.overrideOverload;
 public class OverloadedUsageApp {
 
 	public static void main(final String[] args) {
-		// Create an object libraryOfAlexandria of type Library
-		// Create an object stuttgartOperaHouse of type Opera
+		// Create an object libraryOfAlexandria of type Library and an object stuttgartOperaHouse of type Opera
+		Library libraryOfAlexandria = new Library("libraryOfAlexandria");
+		Opera stuttgartOperaHouse = new Opera("stuttgartOperaHouse");
 
 		// Call this class' static print building info and pass libraryOfAlexandria and
 		// stuttgartOperaHouse
+		printBuildingInfo(libraryOfAlexandria);
+		printBuildingInfo(stuttgartOperaHouse);
 
 		// Create entities building1 and building2 of type Building
 		// let them reference libraryOfAlexandria and stuttgartOperaHouse
+		Building building1 = libraryOfAlexandria;
+		Building building2 = stuttgartOperaHouse;
 
 		// Call this class' static print building info and pass each of the building
 		// entities
+		printBuildingInfo(building1);
+		printBuildingInfo(building2);
 	}
 
 	private static void printBuildingInfo(final Building building) {
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/presence/overrideOverload/OverrideUsageApp.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/presence/overrideOverload/OverrideUsageApp.java
@@ -8,12 +8,20 @@ public class OverrideUsageApp {
 	public static void main(final String[] args) {
 		// Create an object stuttgartOperaHouse of type Opera
 		// Create an object libraryOfAlexandria of type Library
+		Library libraryOfAlexandria = new Library("libraryOfAlexandria");
+		Opera stuttgartOperaHouse = new Opera("stuttgartOperaHouse");
 
 		// Call print building info on both of these entities
+		libraryOfAlexandria.printBuildingInfo();
+		stuttgartOperaHouse.printBuildingInfo();
 
 		// Create entities building1 and building2 of type Building
-		// let them reference libraryOfAlexandria and stuttgartOperaHouse
+		// let them reference libraryOfAlexandria and stuttgartOperaHouse		
+		Building building1 = libraryOfAlexandria;
+		Building building2 = stuttgartOperaHouse;
 
 		// Call print building info on both of these entities
+		building1.printBuildingInfo();
+		building2.printBuildingInfo();
 	}
 }
