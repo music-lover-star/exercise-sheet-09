e1237750946270143a009d85fb385c59371d1fc4
music-lover-star <faridaakrab@gmail.com>
2024-12-20 14:58:30 +0100

    Update FeedOnceStrategy.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/homework/olympics/FeedOnceStrategy.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/homework/olympics/FeedOnceStrategy.java
@@ -6,8 +6,18 @@ package de.unistuttgart.iste.sqa.pse.sheet09.homework.olympics;
  * @author your name
  */
 public final class FeedOnceStrategy implements FeedingStrategy {
-	// if required, put attributes for task (d) here
-
+	private  boolean hasFed = false;
+	@Override
+	public boolean shouldFeed(RunnerHamster hamster) {
+		if (hamster.isAtFeedZone()) && !hasFed) {
+			hasFed = true;
+			return true;
+		}
+		if (!hamster.isAtFeedZone()) {
+			hasFed = false;
+		}
+		return false;
+	}
 	@Override
 	public boolean isFeedingRequired() {
 		// put code for task (d) here
